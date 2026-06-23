e133c245875351be9b3cb5c986365ca12b739d66
ambiit <116333813+ambiit@users.noreply.github.com>
2026-06-23 14:16:35 +0900

    Update mpv.rb

--- mpv.rb
+++ mpv.rb
@@ -1,20 +1,29 @@
 class Mpv < Formula
   desc "Media player based on MPlayer and mplayer2"
   homepage "https://mpv.io"
-  url "https://github.com/mpv-player/mpv/archive/refs/tags/v0.41.0.tar.gz"
-  sha256 "ee21092a5ee427353392360929dc64645c54479aefdb5babc5cfbb5fad626209"
-  license :cannot_represent
-  revision 4
+  license all_of: ["GPL-2.0-or-later", "LGPL-2.1-or-later"]
+  revision 6
   compatibility_version 1
   head "https://github.com/mpv-player/mpv.git", branch: "master"
 
+  stable do
+    url "https://github.com/mpv-player/mpv/archive/refs/tags/v0.41.0.tar.gz"
+    sha256 "ee21092a5ee427353392360929dc64645c54479aefdb5babc5cfbb5fad626209"
+
+    # Backport support for Vapoursynth 74+
+    patch do
+      url "https://github.com/mpv-player/mpv/commit/75b2ccfeb1ce4ed5a40ac9860fa74f3d1265e13f.patch?full_index=1"
+      sha256 "3906b98b02071a0d5747a400406494ca69cef7afd8d3eee4a99fdbe40dc90c1f"
+    end
+  end
+
   bottle do
-    sha256               arm64_tahoe:   "d82d7d7bd6619371bd9fb273c8387c7c7bb34e3e37bc8b6bb68e0622bde55bcd"
-    sha256               arm64_sequoia: "b2eafc6bc8c0265d2ce356ba3819a058a06568a00d19c1cdb01b38a9a1036892"
-    sha256               arm64_sonoma:  "53e833faa61805c10bd1c3bf20bc95198e03b701eeb4c544d0c0fbc0f1f4fb00"
-    sha256 cellar: :any, sonoma:        "b6dd374d5896f71570fa116ab17c448f2d5dd7c7b2e824515fcb120b97c11467"
-    sha256               arm64_linux:   "fc94e7920c2ec65875c212cdb01259f12e94f88976e990ea2706a9289584240e"
-    sha256               x86_64_linux:  "e6af765e0c6d1ec79da369f749d6439306fbe7e98bf34fa055674e433e08a1a3"
+    sha256               arm64_tahoe:   "649109b87d486e369e3ee9c8b6703c3cb17229476881b4b509ca91f0441a17f2"
+    sha256               arm64_sequoia: "484dec512afcfcc30f51aad01022b1bd97aac0fe7c85aafc3b341a25996dd49c"
+    sha256               arm64_sonoma:  "5dd9a950ca0a81ad4319a3f2a203b6e0bd7142b6dc597859ea4acf6dabfadbab"
+    sha256 cellar: :any, sonoma:        "6992a25c76e33dda932ddff26da1e75602e9b402bfe9717792ede3989c2c4ac5"
+    sha256               arm64_linux:   "4df7a2cc6a6a73ab89d69635eedddd00e89176423ace64351c7767813f45a579"
+    sha256               x86_64_linux:  "d795ebc793a8cb4c8548a3dc018fac38a33de14465390ec9d66e586e0fe6c7be"
   end
 
   depends_on "docutils" => :build
@@ -64,18 +73,8 @@ class Mpv < Formula
   conflicts_with cask: "stolendata-mpv", because: "both install `mpv` binaries"
 
   def install
-    # LANG is unset by default on macOS and causes issues when calling getlocale
-    # or getdefaultlocale in docutils. Force the default c/posix locale since
-    # that's good enough for building the manpage.
-    ENV["LC_ALL"] = "C"
-
-    # force meson find ninja from homebrew
-    ENV["NINJA"] = which("ninja")
-
-    # libarchive is keg-only
-    ENV.prepend_path "PKG_CONFIG_PATH", Formula["libarchive"].opt_lib/"pkgconfig" if OS.mac?
-
     args = %W[
+      --sysconfdir=#{etc}
       -Dbuild-date=false
       -Dhtml-build=enabled
       -Djavascript=enabled
@@ -84,9 +83,6 @@ class Mpv < Formula
       -Dlibarchive=enabled
       -Duchardet=enabled
       -Dvulkan=enabled
-      --sysconfdir=#{pkgetc}
-      --datadir=#{pkgshare}
-      --mandir=#{man}
     ]
     if OS.linux?
       args += %w[
@@ -98,26 +94,27 @@ class Mpv < Formula
 
     system "meson", "setup", "build", *args, *std_meson_args
     system "meson", "compile", "-C", "build", "--verbose"
+    system "meson", "compile", "-C", "build", "macos-bundle", "--verbose" if OS.mac?
     system "meson", "install", "-C", "build"
+    bash_completion.install share/"bash-completion/completions/mpv"
+    prefix.install "build/mpv.app" if OS.mac?
 
-    if OS.mac?
-      # `pkg-config --libs mpv` includes libarchive, but that package is
-      # keg-only so it needs to look for the pkgconfig file in libarchive's opt
-      # path.
-      libarchive = Formula["libarchive"].opt_prefix
-      inreplace lib/"pkgconfig/mpv.pc" do |s|
-        s.gsub!(/^Requires\.private:(.*)\blibarchive\b(.*?)(,.*)?$/,
-                "Requires.private:\\1#{libarchive}/lib/pkgconfig/libarchive.pc\\3")
-      end
-    end
+    return unless OS.mac?
 
-    bash_completion.install "etc/mpv.bash-completion" => "mpv"
-    zsh_completion.install "etc/_mpv.zsh" => "_mpv"
+    # `pkg-config --libs mpv` includes libarchive, but that package is
+    # keg-only so it needs to look for the pkgconfig file in libarchive's opt
+    # path.
+    libarchive = formula_opt_prefix("libarchive")
+    inreplace lib/"pkgconfig/mpv.pc",
+              /^Requires\.private:(.*)\blibarchive\b(.*?)(,.*)?$/,
+              "Requires.private:\\1#{libarchive}/lib/pkgconfig/libarchive.pc\\3"
+  end
 
-    if OS.mac?
-      system "python3", "TOOLS/osxbundle.py", "build/mpv"
-      prefix.install "build/mpv.app"
-    end
+  def caveats
+    <<~EOS
+      The global configuration directory is now #{pkgetc}/
+      You may need to migrate any data in previous #{pkgetc}/mpv/
+    EOS
   end
 
   test do
